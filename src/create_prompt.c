e36567ae505645d81c30ed92783200ec71e63136
diego <diego.nascimentomartins2@gmail.com>
2022-03-27 19:44:25 -0300

    create_prompt fixed

--- src/create_prompt.c
+++ src/create_prompt.c
@@ -3,29 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   create_prompt.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dienasci <dienasci@student.42sp.org.br >   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 19:14:03 by coder             #+#    #+#             */
-/*   Updated: 2022/03/27 19:52:48 by coder            ###   ########.fr       */
+/*   Updated: 2022/03/27 17:46:20 by dienasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-char	*get_current_dir(void)
-{
-	char	*buffer;
-
-	return (getcwd(buffer, 0));
-}
-
 char	*create_prompt(void)
 {
-	char	*current_dir;
 	char	*prompt;
+	char	buffer[256];
 
-	current_dir = get_current_dir();
-	prompt = ft_strjoin(current_dir, "> ");
-	free(current_dir);
-	return (prompt);	
+	prompt = ft_strjoin(getcwd(buffer, 256), "> ");
+	return (prompt);
 }
