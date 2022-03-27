68cae9508ac0935e6f1cbfd95d6d4c6422a5734a
diego <diego.nascimentomartins2@gmail.com>
2022-03-27 16:19:12 -0300

    wrong header

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dienasci <dienasci@student.42sp.org.br >   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 07:34:31 by dienasci          #+#    #+#             */
-/*   Updated: 2022/03/27 16:17:27 by dienasci         ###   ########.fr       */
+/*   Updated: 2022/03/27 16:18:38 by dienasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@
 
 int		try_run(char **argv);
 char	*get_current_dir(void);
-char	*get_current_dir(void);
+char	*create_prompt(void);
 
 #endif
